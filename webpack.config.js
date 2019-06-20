e7a811d47114a8d11e68dc9dff12526d1b110de5
AlexeyKobevko <folco_2000@mail.ru>
2019-06-20 15:58:35 +0300

    add server

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-/*jshint esversion: 8 */
-
 const path = require('path');
-const HtmlWebpackPlagin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractLoader = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.[chunkhash].js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node-modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractLoader.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[chunkhash].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractLoader.loader,
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new MiniCssExtractLoader({
-            filename: 'style.[contenthash].css',
-        }),
-        new HtmlWebpackPlagin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-            filename: 'index.html',
-        }),
+      },
     ],
-};
+  },
+  plugins: [
+    new MiniCssExtractLoader({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+}
